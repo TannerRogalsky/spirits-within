0a83ff20d0282cb8a01b2bed0295c794724c7b13
Tanner Rogalsky <tanner@tannerrogalsky.com>
2022-05-03 06:30:27 -0400

    semi-automated deploy

--- .config/webpack.common.js
+++ .config/webpack.common.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const dist = path.resolve(__dirname, "..", "docs");
+const dist = path.resolve(__dirname, "..", "dist");
 
 module.exports = {
   entry: './index.js',
